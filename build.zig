2462ac4c5b3017adfce8c0565e83a10b30b7b53b
LouisAlbanZiko <louisalbanziko1610@gmail.com>
2025-03-30 12:22:20 +0200

    Added exe_name as a parameter to build script

--- build.zig
+++ build.zig
@@ -4,17 +4,20 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const exe_name: []const u8 = "louisalbanziko.com";
+
     const http_server = b.dependency("http_server", .{
         .target = target,
         .optimize = optimize,
         .web_dir = b.path("www"),
+        .exe_name = exe_name,
     });
 
     b.getInstallStep().dependOn(http_server.builder.getInstallStep());
 
     b.installFile("config.zon", "config.zon");
 
-    const exe = http_server.artifact("server_exe");
+    const exe = http_server.artifact(exe_name);
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
