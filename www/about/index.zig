f2ba22c09796593f8941bf99e1ad1afb34a6f5a4
LouisAlbanZiko <louisalbanziko1610@gmail.com>
2025-04-10 13:43:44 +0200

    Added about page (WIP)

---
+++ www/about/index.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const server = @import("server");
+const http = server.http;
+
+const common = @import("/common.zig");
+
+pub fn http_GET(ctx: *http.Context, _: *const http.Request, res: *http.Response) std.mem.Allocator.Error!void {
+    res.code = ._200_OK;
+    try ctx.template(res.body_writer(), @embedFile("index.html.template"), .{
+        .global_css = common.global_css,
+        .global_js = common.global_js,
+        .header = common.header,
+    });
+}
