623c01373df7f7f12d03e854d761069703f0cdbe
LouisAlbanZiko <louisalbanziko1610@gmail.com>
2025-05-15 16:25:43 +0200

    Added zig_cmark as dependency

--- build.zig
+++ build.zig
@@ -4,36 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const cmark = b.addModule("cmark", .{
+    const cmark = b.dependency("cmark", .{
         .target = target,
         .optimize = optimize,
-        .root_source_file = b.path("cmark.zig"),
     });
-    cmark.link_libc = true;
-    cmark.addIncludePath(b.path("cmark/src"));
-    cmark.addCSourceFiles(.{
-        .language = .c,
-        .files = &[_][]const u8{
-            "cmark/src/blocks.c",
-            "cmark/src/buffer.c",
-            "cmark/src/cmark.c",
-            "cmark/src/cmark_ctype.c",
-            "cmark/src/houdini_href_e.c",
-            "cmark/src/houdini_html_e.c",
-            "cmark/src/houdini_html_u.c",
-            "cmark/src/commonmark.c",
-            "cmark/src/html.c",
-            "cmark/src/inlines.c",
-            "cmark/src/iterator.c",
-            "cmark/src/latex.c",
-            "cmark/src/node.c",
-            "cmark/src/references.c",
-            "cmark/src/render.c",
-            "cmark/src/scanners.c",
-            "cmark/src/utf8.c",
-            "cmark/src/xml.c",
-        },
-    });
+    const cmark_mod = cmark.module("cmark");
 
     const exe_name: []const u8 = "louisalbanziko.com";
 
@@ -46,7 +21,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const mod_cmark = hermes.module("/cmark.zig");
-    mod_cmark.addImport("cmark", cmark);
+    mod_cmark.addImport("cmark", cmark_mod);
 
     b.getInstallStep().dependOn(hermes.builder.getInstallStep());
 
--- mods/cmark.zig
+++ mods/cmark.zig
@@ -1,2 +1,25 @@
 const cmark = @import("cmark");
+//pub fn markdown_to_html(md: [:0]const u8) struct {
+//    navigation: [:0]u8,
+//    html: [:0]u8,
+//} {
+//const doc = c.cmark_parse_document(md.ptr, md.len, 0);
+//defer c.cmark_node_free(doc);
+//
+//const iter = c.cmark_iter_new(doc);
+//defer c.cmark_iter_free(iter);
+//
+//var last_level: usize = 1;
+//while (c.cmark_iter_next(iter) != c.CMARK_EVENT_DONE) {
+//    const node = c.cmark_iter_get_node(iter);
+//    const node_type: NodeType = @enumFromInt(node.?.*.type);
+//    if (node_type == .CMARK_NODE_HEADING) {
+//        const node_heading = node.?.*.as.heading;
+//        if (node_heading.level > last_level) {
+//
+//        }
+//        std.debug.print("{d},{d},{}\n", .{ node_heading.internal_offset, node_heading.level, node_heading.setext });
+//    }
+//    std.debug.print("{s}\n", .{@tagName(node_type)});
+//}
 pub const markdown_to_html = cmark.markdown_to_html;
